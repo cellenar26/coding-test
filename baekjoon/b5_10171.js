3e3c0ba5b1aecbc320a94289562de5a8cacbb6ed
cellenar26 <cellenar26@gmail.com>
2022-02-14 21:04:28 +0900

    solve: 2557, 10171, 10172, 10718

---
+++ baekjoon/b5_10171.js
@@ -0,0 +1,8 @@
+const a = '\\    /\\'
+const b = ' )  ( ' + "')"
+const c = '(  /  )'
+const d = ' \\(__)|'
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
---
+++ baekjoon/b5_10172.js
@@ -0,0 +1,10 @@
+const a = '|\\_/|'
+const b = '|q p|   /}'
+const c = '( 0 )"""\\'
+const d = '|'+'"^"`'+'    |'
+const e = '||_/=\\\\__|'
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
